7d4d46e1f2cad75cb48a4cddf4ae7a212a064bb4
Eduardo Costa <eduardocostaprofessor@gmail.com>
2019-11-29 20:32:17 -0300

    Inicialização do projeto vazio

---
+++ src/lib/funcoes.js
@@ -0,0 +1,7 @@
+export function soma(a, b) {
+    return a + b
+}
+
+export function sub(a, b) {
+    return a - b
+}
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+let path = require('path')
+module.exports = {
+    entry : "./src/main.js",//arquivo de entrada
+    output : {//arquivo de saída
+        path: path.join(__dirname, '/public') ,
+        filename : "bundle.js",
+    }, 
+    devServer : {//onde o servidor será montado
+        contentBase: path.join(__dirname, '/public') 
+    },
+    module : {
+        rules : [
+            {
+                test : /\.js$/,
+                exclude : /node_modules/,
+                use : {
+                    loader : 'babel-loader',
+                }
+            }
+        ]
+    }
+}
